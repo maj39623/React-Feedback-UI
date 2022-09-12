0c5fe81b80f6374aefd0d89445cb7e09d9ca6137
Jamar Riley <maj39623@gmail.com>
2022-09-12 07:58:07 -0400

    commit 1

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
+
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
+}
+
+FeedbackList.propTypes = {
+  item: PropTypes.array,
+};
+
+export default FeedbackList;
